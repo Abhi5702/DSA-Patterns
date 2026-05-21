45bc5079de3cfe0557b7cfaca078261603f4ac12
abhi5702 <abhinavprasad267@gmail.com>
2026-05-21 23:59:22 +0530

    kadene

---
+++ ArrayTraversal/MaximumSubArraySum.java
@@ -0,0 +1,29 @@
+package ArrayTraversal;
+
+public class MaximumSubArraySum {
+    
+    public static int maxSubArraySum(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int num : arr) {
+            currentSum += num;
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,4,-1,2,1,5,4};
+        int result = maxSubArraySum(arr);
+        System.out.println("Maximum subarray sum is: " + result);
+    }
+}
--- ArrayTraversal/RemoveDuplicatesUnSorted.java
+++ ArrayTraversal/RemoveDuplicatesUnSorted.java
@@ -17,6 +17,8 @@ public class RemoveDuplicatesUnSorted {
 
     public static void main(String[] args) {
 
+        
+
         int[] arr = {1,2,3,2,4,1,5};
 
         removeDuplicates(arr);
